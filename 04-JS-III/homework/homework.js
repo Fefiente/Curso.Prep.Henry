92b8d7c2a885e8c092ceef28db38157b042bef8d
Fefiente <canterosagustin10@gmail.com>
2020-10-16 17:34:13 -0300

    Tarea 3.1

--- 04-JS-III/homework/homework.js
+++ 04-JS-III/homework/homework.js
@@ -9,7 +9,7 @@ function devolverPrimerElemento(array) {
 function devolverUltimoElemento(array) {
   // Devuelve el último elemento de un array
   // Tu código:
-  return array[array.length -1];aaa
+  return array[array.length -1];aaaa
 } 
 
 function obtenerLargoDelArray(array) {
